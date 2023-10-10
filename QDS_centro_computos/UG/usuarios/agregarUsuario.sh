2000338ac1965133e1a018354deb1fbb16a6b5ca
datavision000 <datavisionuy@gmail.com>
2023-10-10 13:12:24 -0300

    Versión 3.6

--- QDS_centro_computos/UG/usuarios/agregarUsuario.sh
+++ QDS_centro_computos/UG/usuarios/agregarUsuario.sh
@@ -12,111 +12,94 @@ tput setaf 3
 tput cup 3 0; echo "Nombre de usuario: "
 tput cup 4 0; echo "Nombre del grupo: "
 tput cup 5 0; echo "Mail: "
-tput cup 7 0; tput setaf 2; echo "(Ingrese 0 en cualquier campo para regresar...)";
+tput cup 7 0; tput setaf 2; echo "(Ingrese 0 en cualquier campo para regresar...)"
 tput setaf 7
 
-tput cup 3 19; read usu
-
-if [[ $usu != 0 ]]
-then
+while [ true ]
+do
+	tput cup 3 19; echo "                                                   "
+	tput setaf 7; tput cup 3 19; read usu
 
 	usuLetras=$(echo "${#usu}")
-
-	while [ $usuLetras -lt 3 ]
-	do
-		tput setaf 1; tput cup 8 0; echo "El nombre de usuario debe contener al menos 3 caracteres..."
-		tput setaf 7
-       	tput cup 3 19; echo "                                              "
-		tput cup 3 19; read usu
-		usuLetras=$(echo "${#usu}")
-	done
-	
-	tput cup 8 0; echo "                                                                                 "
-
-	while [[ $usu =~ ^[0-9]+$ ]]
-	do
-		tput setaf 1; tput cup 8 0; echo "El nombre de usuario no puede contener numeros..."
-		tput setaf 7
-		tput cup 3 19; echo "                                          "
-		tput cup 3 19; read usu
-	done
-	
-	tput cup 8 0; echo "                                                                                 "
-
 	usu2=$(cut -d: -f1 /etc/passwd | grep -w "$usu")
 
-	while [[ $usu2 != "" ]]
-	do
+	if [[ $usu == 0 ]]
+	then
+		break
+	elif [ $usuLetras -lt 3 ]
+	then
+		tput cup 8 0; echo "                                                                  "
+		tput setaf 1; tput cup 8 0; echo "El nombre de usuario debe contener al menos 3 caracteres..."
+		usuLetras=$(echo "${#usu}")
+	elif [[ $usu =~ [0-9] ]]
+	then
+		tput cup 8 0; echo "                                                                  "
+		tput setaf 1; tput cup 8 0; echo "El nombre de usuario no puede contener numeros..."
+	elif [[ $usu2 != "" ]]
+	then
 		tput cup 8 0; echo "                                                               "
        	tput setaf 1; tput cup 8 0; echo "El usuario '$usu' ya existe en el sistema..."
 		tput setaf 7
-       	tput cup 3 19; echo "                                                   "
-		tput cup 3 19; read usu
-		usu2=$(cut -d: -f1 /etc/passwd | grep -w "$usu")
-	done
+	else
 
-	tput cup 8 0; echo "                                                                      "
+		tput cup 8 0; echo "                                                                  "
+		tput cup 9 0; echo "                                                                  "
 
-	tput cup 4 18; read grupo
-	
-	if [[ $grupo != 0 ]]
-	then
-
-		grupo2=$(cut -d: -f1 /etc/group | grep -w "$grupo")
-
-		while [ -z $grupo2 ]
+		while [ true ]
 		do
-			tput cup 8 0; echo "                                                             "
-       		tput setaf 1; tput cup 8 0; echo "El grupo '$grupo' no existe en el sistema..."
-			tput cup 9 0; echo "Intente ingresando uno existente o regrese luego de crearlo."
-			tput setaf 7
-       		tput cup 4 18; echo "                                                              "
-			tput cup 4 18; read grupo
+			tput cup 4 18; echo "                                                   "
+			tput setaf 7; tput cup 4 18; read grupo
 			grupo2=$(cut -d: -f1 /etc/group | grep -w "$grupo")
-		done
 
-		tput cup 8 0; echo "                                                             "
-		tput cup 9 0; echo "                                                           "
-
-		tput cup 5 6; read mail
-	
-		if [[ $mail != 0 ]]
-		then
-	
-			mail2=$(grep -w ":$mail" /etc/mails)
-
-			while [ $mail2 ]
-			do
-				tput setaf 1; tput cup 8 0; echo "Ese correo ya esta registrado en el sistema..."
-				tput cup 9 0; echo "Ingrese uno diferente."
-				tput setaf 7; tput cup 5 6; echo "                                        "
-				tput cup 5 6; read mail
-				mail2=$(grep -w ":$mail" /etc/mails)
-			done
-
-			tput cup 8 0; echo "                                                                 "
-			tput cup 9 0; echo "                                                  "
-
-			if [[ $grupo == "admin" ]]
+			if [[ $grupo == 0 ]]
 			then
-				echo "$usu ALL=(ALL:ALL) ALL" >> /etc/sudoers
+				break
+			elif [ -z $grupo2 ]
+			then
+				tput cup 8 0; echo "                                                             "
+				tput setaf 1; tput cup 8 0; echo "El grupo '$grupo' no existe en el sistema..."
+				tput cup 9 0; echo "Intente ingresando uno existente o regrese luego de crearlo."
+			else
+		
+				tput cup 8 0; echo "                                                                  "
+				tput cup 9 0; echo "                                                                  "
+
+				while [ true ]
+				do
+					tput cup 5 6; echo "                                       "
+					tput cup 5 6; read mail
+					mail2=$(grep -w ":$mail" /etc/mails)
+
+					if [[ $mail == 0 ]]
+					then
+						break
+					elif [ $mail2 ]
+					then
+						tput setaf 1; tput cup 8 0; echo "Ese correo ya esta registrado en el sistema..."
+						tput cup 9 0; echo "Ingrese uno diferente."
+					else
+						if [[ $grupo == "admin" ]]
+						then
+							echo "$usu ALL=(ALL:ALL) ALL" >> /etc/sudoers
+						fi
+
+						useradd -g $grupo -c $comentario $usu
+						echo "$usu:$usu" | sudo chpasswd
+						echo "$usu:$mail" >> /etc/mails
+						tput cup 7 0; echo "                                                        "
+						tput setaf 2; tput cup 8 0
+						echo "El usuario '$usu' (grupo: '$grupo') fue ingresado correctamente al sistema!"
+						tput cup 9 0; echo "Presione cualquier tecla para volver..."; tput setaf 7; read espera; break 3
+					fi
+
+				done
+
 			fi
 
-			useradd -g $grupo -c $comentario $usu
-			echo "$usu:$usu" | sudo chpasswd
-			echo "$usu:$mail" >> /etc/mails
-			tput cup 7 0; echo "                                                        "
-			tput setaf 2; tput cup 8 0
-			echo "El usuario '$usu' (grupo: '$grupo') fue ingresado correctamente al sistema!"
-			tput cup 9 0; echo "Presione cualquier tecla para volver..."; tput setaf 7; read espera
-	
-		fi
-	
+		done
+
 		
+	fi	
+done
 
-	
-	fi
 
-	
-	
-fi
